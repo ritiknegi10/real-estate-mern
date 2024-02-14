53b14d127c9199f752a65edf46c8d48b6e8a45c2
Ritik <negiritik47@gmail.com>
2024-02-14 21:43:26 +0530

    show user listing functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -39,12 +39,12 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id != req.params.id)
-    return next(errorHandler(401, "You can only delte your own account ! "));
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your own account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token");
-    res.status(200).json("User has been deleted");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
@@ -53,12 +53,12 @@ export const deleteUser = async (req, res, next) => {
 export const getUserListings = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
-      const listing = await Listing.find({ userRef: req.params.id });
+      const listings = await Listing.find({ userRef: req.params.id });
       res.status(200).json(listings);
     } catch (error) {
       next(error);
     }
   } else {
-    return next(errorHandler(401, "You can only view your own Listing"));
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
