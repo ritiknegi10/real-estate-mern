3a06f9126686e6cad9150d29839a6e21c00d8b3e
Ritik <negiritik47@gmail.com>
2024-02-18 14:41:19 +0530

    search api route completed

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,8 @@ import {
   createListing,
   deleteListing,
   updateListing,
+  getListings,
+  getListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +13,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
-
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 export default router;
