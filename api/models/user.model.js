d2ba3a122c3ab153902c50df676880354d2043c4
Ritik <negiritik47@gmail.com>
2024-02-20 23:12:32 +0530

    few other changes

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,11 +19,12 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://media.istockphoto.com/id/1316420668/vector/user-icon-human-person-symbol-social-profile-icon-avatar-login-sign-web-user-symbol.jpg?s=612x612&w=0&k=20&c=AhqW2ssX8EeI2IYFm6-ASQ7rfeBWfrFFV4E87SaFhJE=",
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     },
   },
   { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
+
 export default User;
